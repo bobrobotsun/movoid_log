b544a53f363d330590de69bd816da8339a53654e
sunyifan0649.102.115 <JC0649@pmish-tech.com>
2024-10-29 20:09:44 +0800

    添加analyse value的测试用例

--- test/test_logger.py
+++ test/test_logger.py
@@ -8,7 +8,10 @@
 """
 import time
 
+import pytest
+
 from movoid_log import LoggerBase
+from movoid_log.logger import analyse_value
 
 
 class TestLogger:
@@ -21,3 +24,19 @@ class TestLogger:
         for i in range(20):
             time.sleep(0.1)
             log1.print('asdf')
+
+
+class Test_function_analyse_value:
+    @pytest.mark.parametrize("input_value, return_str", [
+        ('123', '>123<(str)'),
+        (23, '>23<(int)'),
+        (6.87, '>6.87<(float)'),
+        (True, '>True<(bool)'),
+        (None, '>None<(NoneType)'),
+        ((1, 2), '>(1, 2)<(tuple)'),
+        ([0, 3], '>[0, 3]<(list)'),
+        ({8}, '>{8}<(set)'),
+        ({6: 66}, '>{6: 66}<(dict)'),
+    ])
+    def test_input_return(self, input_value, return_str):
+        assert analyse_value(input_value) == return_str
